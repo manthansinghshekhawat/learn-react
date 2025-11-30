1851c545841e9a0f37dfc99b6297fc6103bb4ed1
Manthan Shekhawat <manthanshekhawat@Manthans-MacBook-Air.local>
2025-11-30 17:23:59 +0530

    created first component

--- start/src/App.jsx
+++ start/src/App.jsx
@@ -1,24 +1,12 @@
 import "./App.css";
+import Card from "./components/cards.jsx";
 
 function App() {
   return (
     <>
-      <div className="max-w-sm bg-white border border-grey-200 rounded -xl mt-8 shadow overflow-hidden transition-shadow">
-        <img
-          className="object-cover w-full h-50"
-          src="https://images.unsplash.com/photo-1509042239860-f550ce710b93"
-          alt="Sample Image"
-        ></img>
-        <div className="p-4">
-          <h2 className="text-lg font-semibold  text-gray-800">Card Title</h2>
-          <p className="mt-2 text-gray-600 text-sm">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit cum
-            ducimus, hic laboriosam reiciendis dolor?
-          </p>
-          <button className="mt-4 px-4 bg-blue-400 rounded-lg text-white py-2 hover:bg-blue-700">
-            Buy Now
-          </button>
-        </div>
+      <div className="flex gap-4">
+        <Card />
+        <Card />
       </div>
     </>
   );
