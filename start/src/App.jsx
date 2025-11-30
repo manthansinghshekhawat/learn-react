a0506322af24d0fb56c25e2bbe57d94c7cb670b5
Manthan Shekhawat <manthanshekhawat@Manthans-MacBook-Air.local>
2025-11-30 17:03:06 +0530

    created first card

--- start/src/App.jsx
+++ start/src/App.jsx
@@ -2,11 +2,25 @@ import "./App.css";
 
 function App() {
   return (
-    <main>
-      <h1 className="text-blue-600 dark:text-sky-400 border-2 p-4 ">
-        Hello My name is Manthan
-      </h1>
-    </main>
+    <>
+      <div className="max-w-sm bg-white border border-grey-200 rounded -xl mt-8 shadow overflow-hidden transition-shadow">
+        <img
+          className="object-cover w-full h-50"
+          src="https://images.unsplash.com/photo-1509042239860-f550ce710b93"
+          alt="Sample Image"
+        ></img>
+        <div className="p-4">
+          <h2 className="text-lg font-semibold  text-gray-800">Card Title</h2>
+          <p className="mt-2 text-gray-600 text-sm">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit cum
+            ducimus, hic laboriosam reiciendis dolor?
+          </p>
+          <button className="mt-4 px-4 bg-blue-400 rounded-lg text-white py-2 hover:bg-blue-700">
+            Buy Now
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
