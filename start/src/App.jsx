bf7202bdb59a07e52018afeeaf6ed5712a721599
Manthan Shekhawat <manthanshekhawat@Manthans-MacBook-Air.local>
2025-11-30 20:27:35 +0530

    how to add the tailwind components

--- start/src/App.jsx
+++ start/src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.css";
 import Card from "./components/cards.jsx";
-
+import Header from "./components/header.jsx";
+import Hero from "./components/Hero.jsx";
 function App() {
   return (
     <>
+      <Hero />
+      <div>
+        <Header />
+      </div>
+
       <div className="flex gap-4">
         <Card title="Python" />
         <Card title="Cython" />
+        <Card title="Dython" />
+        <Card title="Rython" />
       </div>
     </>
   );
