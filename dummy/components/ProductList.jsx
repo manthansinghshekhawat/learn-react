0ad13d6bca2caaf9246ffe723ddcccf9d57f7e3a
Manthan Shekhawat <manthanshekhawat@Manthans-MacBook-Air.local>
2025-12-25 12:14:43 +0530

    jsx Practice

---
+++ dummy/components/ProductList.jsx
@@ -0,0 +1,17 @@
+export default function ProductList() {
+  const products = [
+    { id: 1, name: "Phone", price: "$699" },
+    { id: 2, name: "Laptop", price: "$1200" },
+    { id: 3, name: "Headphones", price: "$199" },
+  ];
+  return (
+    <div>
+      {products.map(({ id, name, price }) => (
+        <ul key={id}>
+          <li>{name}</li>
+          <li>{price}</li>
+        </ul>
+      ))}
+    </div>
+  );
+}
