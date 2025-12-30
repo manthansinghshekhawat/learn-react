dcd2c1164fa604bc1d0ee7d86c29b467183c190c
Manthan Shekhawat <manthanshekhawat@Manthans-MacBook-Air.local>
2025-12-30 07:55:37 +0530

    dummy

--- dummy/src/App.jsx
+++ dummy/src/App.jsx
@@ -1,12 +1,12 @@
-import Person from "../components/Person";
-import Product from "../components/Product";
+const Welcome = () => {
+  return <div>Welcome</div>;
+};
 
 function App() {
   return (
-    <>
-      <Person name="Manthan" age={25} />
-      <Product name="Iphone" price={30000} />
-    </>
+    <div>
+      <Welcome />
+    </div>
   );
 }
 
